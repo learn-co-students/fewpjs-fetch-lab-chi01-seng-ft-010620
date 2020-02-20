ba41681a83a48ed29b1dad1aaa978c4f7ae7c745
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-02-20 13:06:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function fetchBooks() {
   return fetch("https://anapioficeandfire.com/api/books")
-    .then(function(response) {
-      return response.json();
-    })
+    .then(resp => resp.json())
     .then(json => renderBooks(json));
 }
 
@@ -10,7 +8,7 @@ function renderBooks(json) {
   const main = document.querySelector("main");
   json.forEach(book => {
     const h2 = document.createElement("h2");
-    h2.innerHTML = `<h2>${f.book.name}</h2>`;
+    h2.innerHTML = `<h2>${book.name}</h2>`;
     main.appendChild(h2);
   });
 }
